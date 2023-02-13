d8b24fa9535f306fe38ac6fd38e461daaf34a10c
serega.maniak@yandex.ru <serega.maniak@yandex.ru>
2023-02-13 20:08:26 +0300

    add new func

--- ProcessShow/Form1.Designer.cs
+++ ProcessShow/Form1.Designer.cs
@@ -38,11 +38,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.lbl_timeStart = new System.Windows.Forms.Label();
             this.lb_procID = new System.Windows.Forms.Label();
             this.lbl_procTime = new System.Windows.Forms.Label();
+            this.lbl_threadcount = new System.Windows.Forms.Label();
+            this.lbl_proccopy = new System.Windows.Forms.Label();
+            this.btn_killproc = new System.Windows.Forms.Button();
+            this.lbl_heshcode = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_createnewproc = new System.Windows.Forms.Button();
+            this.tb_createnewproc = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // listbox_process
@@ -72,7 +78,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(404, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(396, 23);
+            this.button1.Size = new System.Drawing.Size(517, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "TimerIntervalSet";
             this.button1.UseVisualStyleBackColor = true;
@@ -110,9 +116,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(609, 133);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.Size = new System.Drawing.Size(79, 15);
             this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
+            this.label4.Text = "Thread Count";
             // 
             // label5
             // 
@@ -123,23 +129,14 @@
             this.label5.TabIndex = 6;
             this.label5.Text = "Process Time";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(609, 182);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 15);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "label6";
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(609, 158);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 15);
+            this.label7.Size = new System.Drawing.Size(76, 15);
             this.label7.TabIndex = 8;
-            this.label7.Text = "label7";
+            this.label7.Text = "Process copy";
             // 
             // lbl_timeStart
             // 
@@ -165,15 +162,83 @@
             this.lbl_procTime.Size = new System.Drawing.Size(0, 15);
             this.lbl_procTime.TabIndex = 12;
             // 
+            // lbl_threadcount
+            // 
+            this.lbl_threadcount.AutoSize = true;
+            this.lbl_threadcount.Location = new System.Drawing.Point(705, 133);
+            this.lbl_threadcount.Name = "lbl_threadcount";
+            this.lbl_threadcount.Size = new System.Drawing.Size(0, 15);
+            this.lbl_threadcount.TabIndex = 13;
+            // 
+            // lbl_proccopy
+            // 
+            this.lbl_proccopy.AutoSize = true;
+            this.lbl_proccopy.Location = new System.Drawing.Point(705, 158);
+            this.lbl_proccopy.Name = "lbl_proccopy";
+            this.lbl_proccopy.Size = new System.Drawing.Size(0, 15);
+            this.lbl_proccopy.TabIndex = 14;
+            // 
+            // btn_killproc
+            // 
+            this.btn_killproc.Location = new System.Drawing.Point(609, 219);
+            this.btn_killproc.Name = "btn_killproc";
+            this.btn_killproc.Size = new System.Drawing.Size(191, 23);
+            this.btn_killproc.TabIndex = 15;
+            this.btn_killproc.Text = "Завершить процесс";
+            this.btn_killproc.UseVisualStyleBackColor = true;
+            this.btn_killproc.Click += new System.EventHandler(this.btn_killproc_Click);
+            // 
+            // lbl_heshcode
+            // 
+            this.lbl_heshcode.AutoSize = true;
+            this.lbl_heshcode.Location = new System.Drawing.Point(762, 183);
+            this.lbl_heshcode.Name = "lbl_heshcode";
+            this.lbl_heshcode.Size = new System.Drawing.Size(0, 15);
+            this.lbl_heshcode.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(609, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Process HashCode";
+            // 
+            // btn_createnewproc
+            // 
+            this.btn_createnewproc.Enabled = false;
+            this.btn_createnewproc.Location = new System.Drawing.Point(609, 330);
+            this.btn_createnewproc.Name = "btn_createnewproc";
+            this.btn_createnewproc.Size = new System.Drawing.Size(312, 23);
+            this.btn_createnewproc.TabIndex = 18;
+            this.btn_createnewproc.Text = "Запустить новый процесс";
+            this.btn_createnewproc.UseVisualStyleBackColor = true;
+            this.btn_createnewproc.Click += new System.EventHandler(this.btn_createnewproc_Click);
+            // 
+            // tb_createnewproc
+            // 
+            this.tb_createnewproc.Location = new System.Drawing.Point(609, 301);
+            this.tb_createnewproc.Name = "tb_createnewproc";
+            this.tb_createnewproc.Size = new System.Drawing.Size(312, 23);
+            this.tb_createnewproc.TabIndex = 19;
+            this.tb_createnewproc.TextChanged += new System.EventHandler(this.tb_createnewproc_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.tb_createnewproc);
+            this.Controls.Add(this.btn_createnewproc);
+            this.Controls.Add(this.lbl_heshcode);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_killproc);
+            this.Controls.Add(this.lbl_proccopy);
+            this.Controls.Add(this.lbl_threadcount);
             this.Controls.Add(this.lbl_procTime);
             this.Controls.Add(this.lbl_timeStart);
             this.Controls.Add(this.lb_procID);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
@@ -202,10 +267,16 @@
         private Label label3;
         private Label label4;
         private Label label5;
-        private Label label6;
         private Label label7;
         private Label lbl_timeStart;
         private Label lb_procID;
         private Label lbl_procTime;
+        private Label lbl_threadcount;
+        private Label lbl_proccopy;
+        private Button btn_killproc;
+        private Label lbl_heshcode;
+        private Label label8;
+        private Button btn_createnewproc;
+        private TextBox tb_createnewproc;
     }
 }
--- ProcessShow/Form1.cs
+++ ProcessShow/Form1.cs
@@ -8,7 +8,7 @@ namespace ProcessShow
         {
             InitializeComponent();
         }
-
+        Process killprocess = new Process(); 
         private void Form1_Load(object sender, EventArgs e)
         {
             UpdateProcessesList();
@@ -16,8 +16,8 @@ namespace ProcessShow
         public void UpdateProcessesList()
         {
             Process[] processes = Process.GetProcesses();
-                listbox_process.Items.Clear();
-            foreach (Process process in processes) 
+            listbox_process.Items.Clear();
+            foreach (Process process in processes)
             {
                 listbox_process.Items.Add(process.ProcessName);
             }
@@ -35,15 +35,60 @@ namespace ProcessShow
 
         private void lb_process_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(listbox_process.SelectedIndex != -1)
+            if (listbox_process.SelectedIndex != -1)
             {
-                Process[] processesByName = Process.GetProcessesByName(listbox_process.Items[listbox_process.SelectedIndex].ToString());
-                if(processesByName.Length > 0)
+                int orderNumber = 0;
+                int counter = 0;
+                foreach(object obj in listbox_process.Items)
                 {
+                    if((string)obj == listbox_process.Items[listbox_process.SelectedIndex].ToString())
+                    {
+                        if(counter == listbox_process.SelectedIndex) { break; } 
+                        orderNumber++;
+                    }
+                    counter++;
+                }
+                Process[] processesByName = Process.GetProcessesByName(listbox_process.Items[listbox_process.SelectedIndex].ToString());
+                if (processesByName.Length > 0)
+                {
+                    killprocess = processesByName[0];
                     lb_procID.Text = processesByName[0].Id.ToString();
                     lbl_timeStart.Text = processesByName[0].StartTime.ToString("HH:m:s:ffff");
+                    lbl_procTime.Text = processesByName[0].TotalProcessorTime.ToString();
+                    lbl_threadcount.Text = processesByName[0].Threads.Count.ToString();
+                    lbl_proccopy.Text = processesByName.Count().ToString();
+                    lbl_heshcode.Text = processesByName[0].GetHashCode().ToString();
                 }
+                /*foreach (Process proc in processesByName)
+                {
+                    MessageBox.Show(proc.GetHashCode().ToString());
+                }*/
+
             }
+        }
+
+        private void btn_killproc_Click(object sender, EventArgs e)
+        {
+            killprocess.Kill();
+        }
+
+        private void tb_createnewproc_TextChanged(object sender, EventArgs e)
+        {
+            if(tb_createnewproc.Text.Length > 0)
+            {
+                btn_createnewproc.Enabled = true; 
+            }
+            else
+            {
+                btn_createnewproc.Enabled = false; 
+            }
+        }
+
+        private void btn_createnewproc_Click(object sender, EventArgs e)
+        {
+            Process process= new Process();
+            process.StartInfo = new ProcessStartInfo(tb_createnewproc.Text);
+            process.Start();
         }
     }
 }
